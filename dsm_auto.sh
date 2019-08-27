c6b65a611c816a324d01accdf013f8fc3183cbe9
theohartsook <53097335+theohartsook@users.noreply.github.com>
2019-08-27 08:10:22 -0700

    Update dsm_auto.sh

--- dsm_auto.sh
+++ dsm_auto.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# Instructions
-# cd to directory with the USGS .adf and the clip shapefile
 NAME=$1 # either type the name for the folder when calling the script
         # or replace $1 with the name you want
 OUTPATH=/home/theo/Documents/P4_RTK/DJI/DSM/$NAME
 USGS=w001001.adf
-MASK=clipbox.shp
-DSM=test.tif
+MASK=pv1.shp
+TIF=$NAME.tif
+
+# cd to directory with the USGS .adf and the clip shapefile
+cd /home/theo/Documents/P4_RTK/Paradise\ Valley/grdn42w118_13
 
 if [ -d "$OUTPATH" ]
 then
     echo "$NAME already exists."
 else
     mkdir $OUTPATH
-    gdalwarp -cutline $MASK -crop_to_cutline $USGS $OUTPATH/$DSM
-    listgeo -tfw $OUTPATH/$DSM
+    gdalwarp -cutline $MASK -crop_to_cutline -co TFW=YES $USGS $OUTPATH/$TIF
 fi
 
 # TODO
 # better way to get to .adf files
 # automatically make mask polygon
+# add exception handling for duplicate names
