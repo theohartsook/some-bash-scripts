2a9644d5f15c19d47be5ddfb9d51b3f0c60c2be3
theohartsook <53097335+theohartsook@users.noreply.github.com>
2019-10-08 10:24:38 -0700

    Update als_tiling.sh

--- als_tiling.sh
+++ als_tiling.sh
@@ -4,15 +4,17 @@ cd /data/gpfs/assoc/gears/scratch/thartsook
 # tile info
 PREFIX="USCAYF20180722f1a1"
 LAS_DIRECTORY=$1
-#"/data/gpfs/assoc/gears/scratch/thartsook/als_test_registration/als_prefire"
 TEMP_DIRECTORY="/data/gpfs/assoc/gears/scratch/thartsook/als_test_registration/registration_temp/"
 BUFFER_DIRECTORY=$2
 mkdir -p $2"/buffered"
 mkdir -p $2"/seamless" 
+mkdir -p $2"/plot_tiles"
 #"/data/gpfs/assoc/gears/scratch/thartsook/als_test_registration/output"
 
 mkdir -p $TEMP_DIRECTORY
-#"/data/gpfs/assoc/gears/scratch/thartsook/als_test_registration/registration_temp"
+
+NUM_CORES=nproc
+PLOTS=/data/gpfs/assoc/gears/scratch/thartsook/als_test_registration/boundaries/plot_40m_buffer.shp
 
 
 singularity exec lastools_build_9_19_license.sif lasindex -i "$LAS_DIRECTORY"/*.las
@@ -45,26 +47,26 @@ singularity exec lastools_build_9_19_license.sif lastile -lof $TEMP_DIRECTORY/co
 rm "$TEMP_DIRECTORY"/conversion.txt
 ls -d "$TEMP_DIRECTORY/"tiles/*.las >> "$TEMP_DIRECTORY"/tiles.txt
 mkdir -p $TEMP_DIRECTORY"/denoise"
-singularity exec lastools_build_9_19_license.sif lasnoise -lof "$TEMP_DIRECTORY"/tiles.txt -remove_noise -olas -odix _denoised -olas -odir $TEMP_DIRECTORY"/denoise"
+singularity exec lastools_build_9_19_license.sif lasnoise -lof "$TEMP_DIRECTORY"/tiles.txt -remove_noise -olas -odix _denoised -cores NUM_CORES -olas -odir $TEMP_DIRECTORY"/denoise"
 	
     
 # Classify ground
 rm "$TEMP_DIRECTORY"/tiles.txt
 ls -d "$TEMP_DIRECTORY/"denoise/*_denoised.las >> "$TEMP_DIRECTORY"/denoised.txt
 mkdir -p $TEMP_DIRECTORY"/ground1"
-singularity exec lastools_build_9_19_license.sif lasground -lof "$TEMP_DIRECTORY"/denoised.txt -olas -odix _ground -ultra_fine -step 3 -odir $TEMP_DIRECTORY"/ground1"
+singularity exec lastools_build_9_19_license.sif lasground -lof "$TEMP_DIRECTORY"/denoised.txt -olas -odix _ground -ultra_fine -step 3 -cores $NUM_CORES -odir $TEMP_DIRECTORY"/ground1"
 	
 # Get height
 rm "$TEMP_DIRECTORY"/denoised.txt
 ls -d "$TEMP_DIRECTORY/"ground1/*_ground.las >> "$TEMP_DIRECTORY"/ground.txt	
 mkdir -p $TEMP_DIRECTORY"/height"
-singularity exec lastools_build_9_19_license.sif lasheight	-lof "$TEMP_DIRECTORY"/ground.txt -drop_below 0 -drop_above 100 -olas -odix _norm -odir $TEMP_DIRECTORY"/height"
+singularity exec lastools_build_9_19_license.sif lasheight	-lof "$TEMP_DIRECTORY"/ground.txt -drop_below 0 -drop_above 100 -cores $NUM_CORES -olas -odix _norm -odir $TEMP_DIRECTORY"/height"
 
 # Classify
 # "-ground_offset 0.2 -olaz"	
 rm "$TEMP_DIRECTORY"/ground.txt
 ls -d "$TEMP_DIRECTORY/"height/*_norm.las >> "$TEMP_DIRECTORY"/norm.txt
-singularity exec lastools_build_9_19_license.sif lasclassify -lof "$TEMP_DIRECTORY"/norm.txt -ground_offset 0.2 -olas -odix _classify -odir "$BUFFER_DIRECTORY"/buffered
+singularity exec lastools_build_9_19_license.sif lasclassify -lof "$TEMP_DIRECTORY"/norm.txt -ground_offset 0.2 -cores $NUM_CORES -olas -odix _classify -odir "$BUFFER_DIRECTORY"/buffered
 
 # Remove buffer for a second set of tiles
 rm "$TEMP_DIRECTORY"/norm.txt
@@ -74,7 +76,10 @@ singularity exec lastools_build_9_19_license.sif lastile -lof "$TEMP_DIRECTORY"/
 # Run lasinfo to prepare for plot extraction
 singularity exec lastools_build_9_19_license.sif lasinfo -lof "$TEMP_DIRECTORY"/buffered.txt
 rm "$TEMP_DIRECTORY"/buffered.txt
-ls -d "$BUFFER_DIRECTORY/"seamless/*seamless.las >> "$TEMP_DIRECTORY"/seamless.txt
-singularity exec lastools_build_9_19_license.sif lasinfo -lof "$TEMP_DIRECTORY"/seamless.txt
+ls -d "$BUFFER_DIRECTORY/"seamless/*seamless.las >> "$BUFFER_DIRECTORY"/seamless.txt
+singularity exec lastools_build_9_19_license.sif lasinfo -lof "$BUFFER_DIRECTORY"/seamless.txt
+
+# Lasclip to get stuff that intersects with the plots (in progress)
+singularity exec lastools_build_9_19_license.sif lasclip -lof "$BUFFER_DIRECTORY"/seamless.txt -poly $PLOTS -interior -odir "$BUFFER_DIRECTORY"/plot_clips
 
 #rm -r "$TEMP_DIRECTORY"/*
