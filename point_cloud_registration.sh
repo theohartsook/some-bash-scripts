577a7d47292183962eb75b15955bc264b8617baa
theohartsook <53097335+theohartsook@users.noreply.github.com>
2019-11-08 14:25:42 -0800

    Update point_cloud_registration.sh

--- point_cloud_registration.sh
+++ point_cloud_registration.sh
@@ -1,69 +1,71 @@
-#!/usr/bin/env bash
+# ALS goes here
+INPUT_ALS_1=/home/theo/Documents/als_test_registration/ferguson/ALS/pre_fire
+INPUT_ALS_2=/home/theo/Documents/als_test_registration/ferguson/ALS/post_fire
 
-# Point cloud registration shell script. The goal is to be able to register
-# every plot multiple times. First year should be tls to als. Second year will
-# tls, uav, and als to first year als.
+# TLS goes here
+INPUT_TLS_1=/home/theo/Documents/als_test_registration/ferguson/raw/pre_fire_tls
+INPUT_TLS_2=/home/theo/Documents/als_test_registration/ferguson/raw/post_fire_tls
+OUTPUT_TLS_1=/home/theo/Documents/als_test_registration/ferguson/TLS/pre_fire
+OUTPUT_TLS_2=/home/theo/Documents/als_test_registration/ferguson/TLS/post_fire
 
-tls_a_dir=/home/theo/Documents/point_cloud_registration_batch_tests/test_tls_a
-tls_a_2_dir=/home/theo/Documents/point_cloud_registration_batch_tests/test_tls_a_2
-als_a_dir=/home/theo/Documents/point_cloud_registration_batch_tests/test_als_a
-uav_b_dir=/home/theo/Documents/point_cloud_registration_batch_tests/test_uav_b
-uav_b_2_dir=/home/theo/Documents/point_cloud_registration_batch_tests/test_uav_b_2
-voxel_dir=/home/theo/Documents/point_cloud_registration_batch_tests/test_voxels
-# This is the real directory on HPC
-#tls_a_dir=/data/gpfs/assoc/gears/lidarchange/TLS_Output/2017/1_LasVer1.2/2_Clipped/2_34x34m
-#tls_a_2_dir=where should this go?
-#tls_b_dir=/data/gpfs/assoc/gears/lidarchange/TLS_Output/2018/1_LasVer1.2/2_Clipped/2_34x34m
-#uav_b_dir= /data/gpfs/assoc/gears/shared_data/staging/sfm_uav_outputs/northern_sierra/2018/pointCloud_output
-#uav_b_2_dir=output for registered uav
-#als_a_dir=
-#als_b_dir= /data/gpfs/assoc/gears/shared_data/rsdata/lidar_airborne/L1B/vendor/ASO/Ferguson (before-fire)/2018/corrected_flightlines/USCAYF20180722f1a1/2-Correction
+# UAV goes here
+#INPUT_UAV_1
+INPUT_UAV_2=/home/theo/Documents/als_test_registration/ferguson/raw/post_fire_uav
+#OUTPUT_UAV_2
+OUTPUT_UAV_2=/home/theo/Documents/als_test_registration/ferguson/UAV/post_fire
 
-cd $tls_a_dir
-# register tls_a to als_a
+TEMP_DIRECTORY=/home/theo/Documents/als_test_registration/ferguson/temp
+mkdir -p $TEMP_DIRECTORY
+
+cd $INPUT_TLS_1
+mkdir -p "$OUTPUT_TLS_1/"las
+mkdir -p "$OUTPUT_TLS_1/"registration_matrix
+
 for filename in *.las; do
     plot_id=${filename:0:4}
-    als_ref=$plot_id"_ALS_clip.las"
-    cloudcompare.CloudCompare -C_EXPORT_FMT LAS -O $filename -O $als_a_dir/$als_ref -ICP -MIN_ERROR_DIFF 1e-8 -ADJUST_SCALE -RANDOM_SAMPLING_LIMIT 1000000000
-
-    #echo "Export newly registered tls to tls_a_2_dir"
-    #echo "Cloudcompare registration of newly registered tla to tls_b, uav_b, als_b"
-    #echo "Export registered point cloud"
-    #echo "Point cloud comparisons"
+    als_ref="$plot_id"_ALS.las
+    cloudcompare.CloudCompare -SILENT -C_EXPORT_FMT LAS -O $filename -O "$INPUT_ALS_1"/"$als_ref" -ICP -MIN_ERROR_DIFF 1e-8 -RANDOM_SAMPLING_LIMIT 20000
+    mv "$plot_id"*_REGISTERED*.las "$OUTPUT_TLS_1"/las
+    mv "$plot_id"*_REGISTRATION*.txt "$OUTPUT_TLS_1"/registration_matrix
 done
 
-# export registered tls_a
-for filename in *_REGISTERED_*.las; do
-    plot_id=${filename:0:4}
-    output_filename=$plot_id"_TLS_a_2.las"
-    mv $filename $tls_a_2_dir/$output_filename
-done
+cd $INPUT_TLS_2
+mkdir -p "$OUTPUT_TLS_2/"las
+mkdir -p "$OUTPUT_TLS_2/"registration_matrix
 
-# merge uav .laz files
-cd /home/theo/Documents/point_cloud_registration_batch_tests/test_uav_b
-for filename in */; do
-    plot_id=${filename:0:4}
-    WINEDEBUG=-all wine /APPS/LASTools/bin/lasmerge -i *.laz -o $plot_id"_merged.las"
-
-# register uav_b to registered tls (tls_a_2)
-cd $tls_a_2_dir
 for filename in *.las; do
     plot_id=${filename:0:4}
-    uav_b_ref=$plot_id"_UAV_clip.las"
-    cloudcompare.CloudCompare -C_EXPORT_FMT LAS -O $uav_b_dir/$uav_b_ref -O $filename -ICP -MIN_ERROR_DIFF 1e-8 -ADJUST_SCALE -RANDOM_SAMPLING_LIMIT 1000000000
+    als_ref="$plot_id"_ALS.las
+    cloudcompare.CloudCompare -SILENT -C_EXPORT_FMT LAS -O $filename -O "$INPUT_ALS_2"/"$als_ref" -ICP -MIN_ERROR_DIFF 1e-8 -RANDOM_SAMPLING_LIMIT 20000
+    mv "$plot_id"*_REGISTERED*.las "$OUTPUT_TLS_2"/las
+    mv "$plot_id"*_REGISTRATION*.txt "$OUTPUT_TLS_2"/registration_matrix
 done
 
-# export registered uav_b
-for filename in *_REGISTERED_*.las; do
+cd $INPUT_UAV_2
+mkdir -p "$OUTPUT_UAV_2/"las
+mkdir -p "$OUTPUT_UAV_2/"registration_matrix
+mkdir -p "$TEMP_DIRECTORY"/uav
+mkdir -p "$TEMP_DIRECTORY"/tls
+
+for filename in *.las; do
     plot_id=${filename:0:4}
-    output_filename=$plot_id"_UAV_b_2.las"
-    mv $filename $uav_b_2_dir/$output_filename
+    tls_ref="$plot_id"*REGISTERED*.las
+    las2las -i $filename -keep_random_fraction 0.1 -odir $TEMP_DIRECTORY -o "$plot_id"_UAV_downsample_01.las
+    las2las -i "$OUTPUT_TLS_2"/$tls_ref -keep_random_fraction 0.1 -odir $TEMP_DIRECTORY -o "$plot_id"_TLS_downsample_01.las
 done
 
+cd "$TEMP_DIRECTORY"/uav
+for filename in *.las; do
+    plot_id=${filename:0:4}
+    tls_ref="$plot_id"_TLS_downsample_01.las
+    cloudcompare.CloudCompare -SILENT -C_EXPORT_FMT LAS -O $filename -O "$INPUT_DIRECTORY"/"$tls_ref" -ICP -MIN_ERROR_DIFF 1e-8 -RANDOM_SAMPLING_LIMIT 60000
+    mv "$plot_id"*_REGISTRATION*.txt "$OUTPUT_UAV_2"/registration_matrix
+done
 
-# TODOs
-# edit the plot id and als_ref to work for 3 digit and 5 digit plot ids
-# update the loop so it cd's into the output directory
-# add wildcard so for loops are .las or .laz
-# rename output files to something specific
-# update to output registration matrix too
+cd $INPUT_UAV_2
+for filename in *.las; do
+    plot_id=${filename:0:4}
+    matrix="$plot_id"*_REGISTRATION*.txt
+    cloudcompare.CloudCompare -SILENT -APPLY_TRANS "$OUTPUT_UAV_2"/registration_matrix/"$matrix"
+    mv "$plot_id"*_REGISTERED*.las "$OUTPUT_UAV_2"/las
+done
