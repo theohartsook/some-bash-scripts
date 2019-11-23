b23c4760dd10d86d74125b12197adce1f8e28a12
theohartsook <53097335+theohartsook@users.noreply.github.com>
2019-11-23 08:37:46 -0800

    Update uav_merge.sh

--- uav_merge.sh
+++ uav_merge.sh
@@ -1,49 +1,23 @@
 # Raw UAV sfm goes here
-INPUT_DIRECTORY=/home/theo/Documents/sfm_merge_test/raw_uav_pc
+INPUT_DIRECTORY=$1
+
+# Merged UAV go here
+OUTPUT_DIRECTORY=$2
 
 # Plot boundary directory goes here
-PLOT_DIRECTORY=/home/theo/Documents/sfm_merge_test/boundaries
-
-# Temp directory
-TEMP_DIRECTORY=/home/theo/Documents/sfm_merge_test/temp
-
-# Merged and clipped UAV go here
-OUTPUT_DIRECTORY=/home/theo/Documents/sfm_merge_test/output
+PLOT_DIRECTORY=$3
 
 # Coordinates for the initial point clouds
-UTM=11n
-#UTM=${4:-10n}
+PREFIX=$4
 
-mkdir -p "$TEMP_DIRECTORY"/merged
-mkdir -p "$TEMP_DIRECTORY"/reproject
-mkdir -p "$OUTPUT_DIRECTORY"/merged
-mkdir -p "$OUTPUT_DIRECTORY"/clipped
+LASTOOLS_SINGULARITY="/data/gpfs/assoc/gears/scratch/thartsook/gears-singularity_gears-lastools.sif"
+
+
+mkdir -p "$OUTPUT_DIRECTORY"/"$PREFIX"
 
 cd $INPUT_DIRECTORY
 
 for directory in */; do
     plot_id=${directory:0:4}
-    lasmerge -i "$directory"/*.laz -o "$plot_id"_UAV_merged.las -odir "$TEMP_DIRECTORY"/merged
+    singularity exec $LASTOOLS_SINGULARITY lasmerge -i "$directory"/*.laz -o "$plot_id"_UAV_merged.las -odir "$OUTPUT_DIRECTORY"/"$PREFIX"
 done
-
-cd $TEMP_DIRECTORY/merged
-for filename in *.las; do
-    plot_id=${filename:0:4}
-    las2las -i $filename -utm 11n -wgs84 -target_epsg 3310 -odir "$TEMP_DIRECTORY"/reproject
-done
-
-cd $TEMP_DIRECTORY/reproject
-for filename in *.las; do
-    plot_id=${filename:0:4}
-    lasnoise -i $filename -remove_noise -odir "$OUTPUT_DIRECTORY"/merged
-done
-
-cd "$OUTPUT_DIRECTORY"/merged
-for filename in *.las; do
-    plot_id=${filename:0:4}
-    lasclip -i $filename -poly "$PLOT_DIRECTORY"/"$plot_id".shp -o "$plot_id"_UAV_clipped.las -odir "$OUTPUT_DIRECTORY"/clipped
-done
-
-# TODOS
-# set projection settings for point cloud merge
-# check that bounding boxes make sense
